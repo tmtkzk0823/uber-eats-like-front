613766c060ed118799b660d7b83ac7078eb4311f
tomita_kazuki <tmtkzk@icloud.com>
2022-09-08 11:23:12 +0900

    Add: レストランコンポーネントのヘッダーを作成

--- src/containers/Restaurants.jsx
+++ src/containers/Restaurants.jsx
@@ -1,5 +1,13 @@
 import React, { Fragment, useEffect } from "react";
+import styled from "styled-components";
+
+//api
 import { fetchRestaurants } from "../apis/restaurants";
+
+//images
+import MainLogo from '../images/logo.png';
+import MainCoverImage from '../images/main-cover-image.png'
+
 
 export const Restaurants = () => {
   useEffect(() => {
@@ -11,7 +19,34 @@ export const Restaurants = () => {
 
   return(
     <Fragment>
-      レストラン一覧
+      <HeaderWrapper>
+        {/* alt属性="" imageが表示できない時に指定したテキストを表示する */}
+        <MainLogoImage src={ MainLogo } alt="main logo"/>
+      </ HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={ MainCoverImage } alt="main cover" />
+      </ MainCoverImageWrapper>
     </Fragment>
   )
 }
+
+// justify-contentはアイテムを主軸（中心の軸）方向に揃え位置を指定する
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`;
+
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`;
+
+const MainCover = styled.img`
+height: 600px;
+`;
+
